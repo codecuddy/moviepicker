1a127277f22e0ed0e33ab713faf0694b44dc1476
Charlie <charliecuddy4@gmail.com>
2019-01-02 01:14:52 -0600

    add title and service to the migrate folder

--- db/migrate/20190102055944_create_movies.rb
+++ db/migrate/20190102055944_create_movies.rb
@@ -1,7 +1,8 @@
 class CreateMovies < ActiveRecord::Migration[5.0]
   def change
     create_table :movies do |t|
-
+      t.string :title
+      t.string :service 
       t.timestamps
     end
   end
