d33199988b011973e4e92bb0af93c35d20e82c47
Charlie <charliecuddy4@gmail.com>
2019-01-02 01:53:20 -0600

    Movies controller associations started, form partial created

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,40 @@
 class MoviesController < ApplicationController
+  before_action :find_movie, only: [:show]
+
   def index
-    @movie = Movie.all
+    @movies = Movie.all
+  end
+
+  def new
+    @movies = Movie.new(movie_params)
+  end
+
+  #def create
+  #@movie = Moview.new(movie_params)
+
+  #  if @movie.save
+  #    redirect_to @movie, notice: "Movie Entered"
+  #  else
+  #    render 'new'
+  #  end
+  # end
+
+  def show
+    @movie = Movie.find_movie
+  end
+
+  #def destroy
+  # @movie.destroy
+  # redirect_to root_path
+  #end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :service)
+  end
+
+  def find_movie
+    @movie = Movie.find(params[:id])
   end
 end
