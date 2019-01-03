4161aaa36bc2224f05844a9df9f174ec57c8f9fe
Charlie <charliecuddy4@gmail.com>
2019-01-02 19:11:17 -0600

    migrate to db/schema, able to show movies saved from console (not from form yet)

--- db/migrate/20190103004134_create_movies.rb
+++ db/migrate/20190103004134_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration[5.0]
   def change
     create_table :movies do |t|
       t.string :title
-      t.string :service 
+      t.string :service
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190102055944) do
+ActiveRecord::Schema.define(version: 20190103004134) do
 
   create_table "movies", force: :cascade do |t|
+    t.string   "title"
+    t.string   "service"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
