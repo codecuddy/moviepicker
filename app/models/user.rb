013975567afbaf411e16333e983fa513b9900071
Charlie <charliecuddy4@gmail.com>
2019-01-03 16:55:21 -0600

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-=======
+
   has_many :movies
->>>>>>> 2cef186d4fa2cb8a47be8e5ce506d3153f2aa1fd
 end
