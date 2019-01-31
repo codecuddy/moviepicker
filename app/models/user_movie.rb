4eea907efb4cb84b0ff917e0d7f1bf324699b81d
Charlie <charliecuddy4@gmail.com>
2019-01-31 14:02:48 -0600

    validate uniqueness of movie for current user

--- app/models/user_movie.rb
+++ app/models/user_movie.rb
@@ -1,5 +1,5 @@
 class UserMovie < ApplicationRecord
   belongs_to :user
   belongs_to :movie
-  validates :movie_id, uniqueness: { scope: @user_movie }
+  validates_uniqueness_of :user_id, :scope => [:movie_id]
 end
