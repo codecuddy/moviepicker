b01a0652f7b2f4a554615cb3458b42702b65c161
Charlie <charliecuddy4@gmail.com>
2019-02-06 01:00:42 -0600

    remove movie from library, leaving it in database

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -1,13 +1,16 @@
 class UserMoviesController < ApplicationController
   #before_action :find_user_movie, only: [:show, :edit, :update, :destroy]
-
+  def show
+  end
+  
   def destroy
-    @user_movie = current_user.movies.find(params[:id])
+    @user_movie = current_user.user_movies.find(params[:id])
     puts "-----------------------------------#{@user_movie.attributes}"
     puts "-----------------------------------#{@user_movie.movie_id}"
-
+    #puts "$$$$$$$$$$$$$$$$$$$$ #{@movie.attributes}"
+    puts "--------------------will destroy USER movie--------------------"
     @user_movie.destroy
-    puts "--------------------destroying USER movie--------------------"
+    puts "--------------------destroyed USER movie--------------------"
     redirect_to root_path
   end
 
