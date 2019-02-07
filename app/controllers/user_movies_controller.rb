eb4d435e151ce719dca95c80b053992d431da1a8
Charlie <charliecuddy4@gmail.com>
2019-02-07 11:35:35 -0600

    style buttons on show movie page

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -1,22 +1,18 @@
 class UserMoviesController < ApplicationController
   #before_action :find_user_movie, only: [:show, :edit, :update, :destroy]
-  def show
-  end
-  
+
+
+  #def show
+    #@user_movie = current_user.user_movies.where(movie_id: @movie.id).first
+  #end
+
   def destroy
     @user_movie = current_user.user_movies.find(params[:id])
     puts "-----------------------------------#{@user_movie.attributes}"
     puts "-----------------------------------#{@user_movie.movie_id}"
-    #puts "$$$$$$$$$$$$$$$$$$$$ #{@movie.attributes}"
     puts "--------------------will destroy USER movie--------------------"
     @user_movie.destroy
     puts "--------------------destroyed USER movie--------------------"
     redirect_to root_path
   end
-
-  private
-
-  #def find_user_movie
-    #@user_movie = current_user.movies.find(params[:id])
-  #end
 end
--- lib/tasks/movies.rake
+++ lib/tasks/movies.rake
@@ -4,7 +4,7 @@ namespace :movies do
   desc "TODO"
   task create_all: :environment do
     # Can only rake < 40 pages of movies at a time to avoid 429 error so change from 1..1000 to 1..30 then 30..60 etc
-    (1000..1010).each do |page_num|
+    (1..4).each do |page_num|
       puts "=======Page Number: #{page_num} of 1,000======="
       response = RestClient.get "https://api.themoviedb.org/3/discover/movie?page="+page_num.to_s+"&api_key="+ENV['MOVIES_DB_API_KEY']
       if response.code == 500
